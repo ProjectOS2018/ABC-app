efc43ec226bd29a659625a19b6382eb20cf51e3e
ProjectOS <Rehab@Rehabs-MacBook-Pro.local>
2018-04-12 18:04:52 +0300

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/alphabet/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package alphabet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.myabcapp.alphabet";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "0.4";
+}
